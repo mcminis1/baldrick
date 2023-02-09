b495492e30c24a1dae74076de57a600733f510b8
Jeremy McMinis <jeremy@sawtoothdata.com>
2023-02-09 15:14:59 -0500

    adding in initial prompts and connecting the tubes

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import os
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from fastapi import FastAPI, Request
-from .openai import get_llm_query
+from .openai import get_relevant_activities, get_sql_query
 from .bigquery import run_query, get_query_plan
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -26,38 +26,39 @@ async def handle_message_events(body, logger):
     logger.debug(body)
     pass
 
-
 class VALID_QUERY_RESPONSE:
-    def __init__(self, llm_query, llm_query_plan, data):
+    def __init__(self, activities, llm_query, llm_query_plan, data):
+        self.activities=activities
         self.llm_query = llm_query
         self.llm_query_plan = llm_query_plan
         self.data = data
     def __str__(self):
-        return f" query:\n {self.llm_query}\n\n query plan:\n {self.llm_query_plan}\n\n results: {self.data}"
+        return f" activities:\n {self.activities}\n\n query:\n {self.llm_query}\n\n query plan:\n {self.llm_query_plan}\n\n results: {self.data}"
     def __repr__(self):
         return str(self)
-
 
 class INVALID_QUERY_RESPONSE:
-    def __init__(self, llm_query):
+    def __init__(self, activities, llm_query):
+        self.activities=activities
         self.llm_query = llm_query
     def __str__(self):
-        return f" query:\n {self.llm_query}\n\n query plan is invalid"
+        return f" activities:\n {self.activities}\n\n query:\n {self.llm_query}\n\n query plan is invalid"
     def __repr__(self):
         return str(self)
-
 
 @app.event("app_mention")
 async def handle_app_mentions(body, say, logger):
     logging.debug(f"/handle_app_mentions")
     logger.debug(body)
-    llm_query = get_llm_query(body["event"]["text"])
-    llm_query_plan = get_query_plan(llm_query)
-    if llm_query_plan:
-        data = run_query(llm_query)
-        await say(VALID_QUERY_RESPONSE(llm_query, llm_query_plan, data))
+    user_question = body["event"]["text"]
+    activities = get_relevant_activities(user_question)
+    query = get_sql_query(user_question, activities)
+    query_plan = get_query_plan(query)
+    if query_plan:
+        data = run_query(query)
+        await say(VALID_QUERY_RESPONSE(activities, query, query_plan, data))
     else:
-        await say(INVALID_QUERY_RESPONSE(llm_query))
+        await say(INVALID_QUERY_RESPONSE(activities, query))
 
 
 api = FastAPI()
