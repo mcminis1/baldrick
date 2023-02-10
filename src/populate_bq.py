67d47848519b944ea73f5a16d0dcfadb49b33009
Miles Tweed <milestweed@gmail.com>
2023-02-10 15:44:12 -0500

    non-conversion ids added

--- src/populate_bq.py
+++ src/populate_bq.py
@@ -9,7 +9,7 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 dataset_id = "baldrick.doodad_inc"
-table_id = dataset_id + ".customer_stream"
+table_id = dataset_id + ".customer_stream_new"
 
 query = f"""
     CREATE TABLE IF NOT EXISTS {table_id}
@@ -36,7 +36,7 @@ num_customers = 200
 num_products = 30
 max_interactions = 100
 min_interactions = 5
-non_conversion_ids = num_customers + 0.33
+non_conversion_ids = int(num_customers + 0.33)
 
 # list of products with price
 products = {
@@ -53,7 +53,7 @@ customers = {
     fake.email(domain=fake.domain_name()): (
         fake.name(),
         fake.address(),
-        fake.date_of_birth(minimum_age=18, maximum_age=88),
+        date.isoformat(fake.date_of_birth(minimum_age=18, maximum_age=88)),
     )
     for _ in range(num_customers)
 }
@@ -62,18 +62,18 @@ customers = {
 pages = ["/contact", "/home", "/about", "/products"]
 
 # activity stream of customer interactions
-cust_acts = []
+cust_acts = list()
 
 
 def ACTIVITY_HANDLER(activity, cust, ts):
 
     if activity == "Visited Page":
-        feats = {"URL": np.random.choice(pages)}
+        feats = json.dumps({"URL": np.random.choice(pages)})
         out = {"activity_id": fake.uuid4(),"ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
   
     elif activity == "Placed Order":
         prod = np.random.choice(list(products.keys()))
-        feats = {"product": prod, "price": products[prod]}
+        feats = json.dumps({"product": prod, "price": products[prod]})
         out = {"activity_id": fake.uuid4(), "ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
 
         if np.random.choice([True, False], p=[1 / 10, 9 / 10]):
@@ -81,44 +81,56 @@ def ACTIVITY_HANDLER(activity, cust, ts):
 
             if ret_ts.date() <= date.today():
                 activity = "Returned Item"
-                feats = {"product": prod, "price": products[prod]}
+                feats = json.dumps({"product": prod, "price": products[prod]})
                 out = {"activity_id": fake.uuid4(), "ts": ret_ts, "customer": cust, "activity": activity, 'feature_json': feats}
 
     else:
-        feats = {"representative": fake.name(), "notes": fake.paragraph()}
+        feats = json.dumps({"representative": fake.name(), "notes": fake.paragraph()})
         out = {"activity_id": fake.uuid4(), "ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
 
-    return json.dumps(out)
+    out['ts'] = date.isoformat(out['ts'])
+
+    return out
+
+activities = ["Visited Page", "Placed Order", "Contacted Support"]
+
+for _ in range(non_conversion_ids):
+
+    anon_id = fake.uuid4()
+    
+    ts = fake.date_time_this_decade()
+    activity = np.random.choice(activities[:2])
+    cust_acts.append(ACTIVITY_HANDLER(activity, anon_id, ts))
 
 
 # Generate DF of randomly generated customer interactions
 for cust, info in customers.items():
     anon_id = fake.uuid4()
-    activities = ["Visited Page", "Placed Order", "Contacted Support"]
 
     ts = fake.date_time_this_decade()
     for _ in range(np.random.randint(low=1, high=5)):
         
         activity = np.random.choice(activities[:2])
-        cust_acts = cust_acts.append(ACTIVITY_HANDLER(activity, anon_id, ts))
+        cust_acts.append(ACTIVITY_HANDLER(activity, anon_id, ts))
 
     init_activity = "Created Account"
     init_features = {"name": info[0], "address": info[1], "birthdate": info[2], 'anon_id': anon_id}
 
-    cust_acts = cust_acts.append(ACTIVITY_HANDLER(init_activity, cust, ts))
+    cust_acts.append(ACTIVITY_HANDLER(init_activity, cust, ts))
 
     for _ in range(np.random.randint(min_interactions, max_interactions)):
         ts = ts + timedelta(days=np.random.randint(1, 11))
         if ts.date() <= date.today():
             activity = np.random.choice(activities, p=[1 / 2, 1 / 4, 1 / 4])
               
-            cust_acts = cust_acts.append(ACTIVITY_HANDLER(activity, cust, ts))
+            cust_acts.append(ACTIVITY_HANDLER(activity, cust, ts))
 
 
 
 job_config = bigquery.LoadJobConfig(
     # Specify a (partial) schema. All columns are always written to the
     # table. The schema is used to assist in data type definitions.
+    # source_format="NEWLINE_DELIMITED_JSON",
     # schema=[
     #     # Specify the type of columns whose type cannot be auto-detected. For
     #     # example the "title" column uses pandas dtype "object", so its
