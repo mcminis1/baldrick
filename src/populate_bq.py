f122850a8987a570f065cfc66f4b5201c4cd5074
Miles Tweed <milestweed@gmail.com>
2023-02-10 14:51:00 -0500

    pandas -> json replacement

--- src/populate_bq.py
+++ src/populate_bq.py
@@ -3,7 +3,6 @@ import json
 
 from faker import Faker
 import numpy as np
-import pandas as pd
 
 from google.cloud import bigquery
 
@@ -63,27 +62,19 @@ customers = {
 pages = ["/contact", "/home", "/about", "/products"]
 
 # activity stream of customer interactions
-cust_act_df = pd.DataFrame(columns=["ts", "customer", "activity", "feature_json"])
+cust_acts = []
 
 
 def ACTIVITY_HANDLER(activity, cust, ts):
 
     if activity == "Visited Page":
         feats = {"URL": np.random.choice(pages)}
-        temp_df = pd.DataFrame(
-            {"ts": ts, "customer": cust, "activity": activity},
-            index=pd.RangeIndex(1),
-        )
-        temp_df["feature_json"] = [feats]
-
+        out = {"activity_id": fake.uuid4(),"ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
+  
     elif activity == "Placed Order":
         prod = np.random.choice(list(products.keys()))
         feats = {"product": prod, "price": products[prod]}
-        temp_df = pd.DataFrame(
-            {"ts": ts, "customer": cust, "activity": activity},
-            index=pd.RangeIndex(1),
-        )
-        temp_df["feature_json"] = [feats]
+        out = {"activity_id": fake.uuid4(), "ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
 
         if np.random.choice([True, False], p=[1 / 10, 9 / 10]):
             ret_ts = ts + timedelta(days=np.random.randint(1, 8))
@@ -91,21 +82,13 @@ def ACTIVITY_HANDLER(activity, cust, ts):
             if ret_ts.date() <= date.today():
                 activity = "Returned Item"
                 feats = {"product": prod, "price": products[prod]}
-                temp_df = pd.DataFrame(
-                    {"ts": ret_ts, "customer": cust, "activity": activity},
-                    index=pd.RangeIndex(1),
-                )
-                temp_df["feature_json"] = [feats]
+                out = {"activity_id": fake.uuid4(), "ts": ret_ts, "customer": cust, "activity": activity, 'feature_json': feats}
 
     else:
         feats = {"representative": fake.name(), "notes": fake.paragraph()}
-        temp_df = pd.DataFrame(
-            {"ts": ts, "customer": cust, "activity": activity},
-            index=pd.RangeIndex(1),
-        )
-        temp_df["feature_json"] = [feats]
-    
-    return temp_df
+        out = {"activity_id": fake.uuid4(), "ts": ts, "customer": cust, "activity": activity, 'feature_json': feats}
+
+    return json.dumps(out)
 
 
 # Generate DF of randomly generated customer interactions
@@ -117,32 +100,20 @@ for cust, info in customers.items():
     for _ in range(np.random.randint(low=1, high=5)):
         
         activity = np.random.choice(activities[:2])
-        cust_act_df = pd.concat([cust_act_df, ACTIVITY_HANDLER(activity, anon_id, ts)], ignore_index=True)
+        cust_acts = cust_acts.append(ACTIVITY_HANDLER(activity, anon_id, ts))
 
     init_activity = "Created Account"
     init_features = {"name": info[0], "address": info[1], "birthdate": info[2], 'anon_id': anon_id}
 
-    temp_df = pd.DataFrame(
-        {"ts": ts, "customer": cust, "activity": init_activity}, index=pd.RangeIndex(1)
-    )
-    temp_df["feature_json"] = [init_features]
-    cust_act_df = pd.concat([cust_act_df, temp_df], ignore_index=True)
+    cust_acts = cust_acts.append(ACTIVITY_HANDLER(init_activity, cust, ts))
 
     for _ in range(np.random.randint(min_interactions, max_interactions)):
         ts = ts + timedelta(days=np.random.randint(1, 11))
         if ts.date() <= date.today():
             activity = np.random.choice(activities, p=[1 / 2, 1 / 4, 1 / 4])
               
-            cust_act_df = pd.concat([cust_act_df, ACTIVITY_HANDLER(activity, cust, ts)], ignore_index=True)
+            cust_acts = cust_acts.append(ACTIVITY_HANDLER(activity, cust, ts))
 
-# Sort data by time and reset index to get an ordered activity_id
-final_df = (
-    cust_act_df.sort_values(by="ts")
-    .reset_index(drop=True)
-    .reset_index()
-    .rename(columns={"index": "activity_id"})
-    .astype({"activity_id": str})
-)
 
 
 job_config = bigquery.LoadJobConfig(
@@ -161,7 +132,7 @@ job_config = bigquery.LoadJobConfig(
 )
 
 # Populate table from pandas DF
-job = client.load_table_from_dataframe(final_df, table_id, job_config=job_config)
+job = client.load_table_from_json(cust_acts, table_id, job_config=job_config)
 job.result()
 
 table = client.get_table(table_id)  # Make an API request.
